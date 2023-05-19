fe33bbf991dad82442e67c1f4eb5abf6852f7138
João Vitor <vitorjoao39207@gmail.com>
2023-05-19 08:49:15 -0300

    Fazendo a função de alterar e excluir

--- app/src/main/java/com/example/crudapp/MainActivity.java
+++ app/src/main/java/com/example/crudapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -19,14 +20,16 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editTitulo, editCorpo;
-    Button btnAdicionar;
+    EditText editTitulo, editCorpo, editId;
+    Button btnAdicionar, btnEditar, btnExcluir;
     ListView listViewPost;
 
     private String HOST = "https://jsonplaceholder.typicode.com";
 
     PostAdapter postAdapter;
     List<Post> lista;
+
+    private int itemClicado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +38,11 @@ public class MainActivity extends AppCompatActivity {
 
         editTitulo = (EditText) findViewById(R.id.editTitulo);
         editCorpo = (EditText) findViewById(R.id.editCorpo);
+        editId = (EditText) findViewById(R.id.editId);
 
         btnAdicionar = (Button) findViewById(R.id.btnSalvar);
+        btnEditar = (Button) findViewById(R.id.btnEditar);
+        btnExcluir = (Button) findViewById(R.id.btnExcluir);
 
         listViewPost = (ListView) findViewById(R.id.listViewPost);
 
@@ -58,7 +64,8 @@ public class MainActivity extends AppCompatActivity {
 
                 if(titulo.isEmpty()) {
                     editTitulo.setError("O titulo é obrigatorio");
-                } else if (corpo.isEmpty()){
+                }
+                else if (corpo.isEmpty()){
                     editCorpo.setError("O corpo é obrigatorio");
                 } else {
                     JsonObject json = new JsonObject();
@@ -73,26 +80,110 @@ public class MainActivity extends AppCompatActivity {
                             .setCallback(new FutureCallback<JsonObject>() {
                                 @Override
                                 public void onCompleted(Exception e, JsonObject result) {
-
-                                    if(result.get("user_id") != null){
-                                        Toast.makeText(MainActivity.this,
-                                                "Salvo sucesso",
-                                                Toast.LENGTH_LONG).show();
-                                    } else {
-                                        Toast.makeText(MainActivity.this,
-                                                "Ocorreu um erro ao salvar",
-                                                Toast.LENGTH_LONG).show();
-                                    }
+                                    Toast.makeText(MainActivity.this,
+                                            "Salvo com sucesso",
+                                            Toast.LENGTH_LONG).show();
                                 }
                             });
+
+                    limparCampos();
                 }
             }
-            public void limparCampos() {
-                editCorpo.setText("");
-                editTitulo.setText("");
+
+        });
+
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String titulo = editTitulo.getText().toString();
+                String corpo = editCorpo.getText().toString();
+                String id = editId.getText().toString();
+
+                String url = HOST + "/posts";
+
+                if(titulo.isEmpty()) {
+                    editTitulo.setError("O titulo é obrigatorio");
+                }
+                else if (corpo.isEmpty()){
+                    editCorpo.setError("O corpo é obrigatorio");
+                }
+                else {
+                    JsonObject json = new JsonObject();
+                    json.addProperty("title", titulo);
+                    json.addProperty("body", corpo);
+                    json.addProperty("userId", id);
+
+                    Ion.with(MainActivity.this)
+                            .load(url)
+                            .setJsonObjectBody(json)
+                            .asJsonObject()
+                            .setCallback(new FutureCallback<JsonObject>() {
+                                @Override
+                                public void onCompleted(Exception e, JsonObject result) {
+                                    Toast.makeText(MainActivity.this,
+                                            "Editado com sucesso",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            });
+
+                    limparCampos();
+                }
             }
         });
 
+        btnExcluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = editId.getText().toString();
+
+                if(id.isEmpty()){
+                    Toast.makeText(MainActivity.this,
+                            "Nenhum contatao selecioando",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    String url = HOST + "/posts";
+
+                    JsonObject json = new JsonObject();
+                    json.addProperty("userId", id);
+
+                    Ion.with(MainActivity.this)
+                            .load(url)
+                            .setJsonObjectBody(json)
+                            .asJsonObject()
+                            .setCallback(new FutureCallback<JsonObject>() {
+                                @Override
+                                public void onCompleted(Exception e, JsonObject result) {
+                                    Toast.makeText(MainActivity.this,
+                                            "Apagado com sucesso",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            });
+
+                    limparCampos();
+                }
+            }
+        });
+
+        listViewPost.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                Post p = (Post) adapterView.getAdapter().getItem(position);
+
+                editTitulo.setText(p.getTitulo());
+                editCorpo.setText(p.getCorpo());
+                editId.setText(p.getId());
+
+                itemClicado = position;
+            }
+        });
+
+
+    }
+    public void limparCampos() {
+        editCorpo.setText("");
+        editTitulo.setText("");
+        editId.setText("");
     }
 
     private  void listaContato(){
